6ddddbf48b1fe8b19a5acf721ba65e669be16396
RolandoPranataAR <rolandopranata@gmail.com>
2022-12-20 04:02:17 +0700

    docs: add rules into eslint configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
