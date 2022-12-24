7e2c29e320ff59d08c005568def8de19de8a9a41
RolandoPranataAR <rolandopranata@gmail.com>
2022-12-24 14:12:01 +0700

    style: add base styling for all page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import { Discover } from './pages';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Discover />} />
-      </Routes>
+    <div className="relative flex">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <div className="px6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
